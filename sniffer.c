7a523e1b80ce4d57f808c1dfc6a7c6427c315856
KristianKicinka <kristian.kicinka@gmail.com>
2022-03-05 17:26:50 +0100

    after add command parameters customization

--- sniffer.c
+++ sniffer.c
@@ -1,9 +1,73 @@
 
 #include "sniffer.h"
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    printf("Hello from the sniffer\n");
+int main(int argc, char *argv[]){
+    SnifferOptions sniffer_options;
+    initialize_sniffer_options(&sniffer_options);
+    check_arguments(argc,argv, &sniffer_options);
+    printf("interface : %s\n",sniffer_options.interface);
+    printf("port number : %d\n",sniffer_options.port_number);
+    printf("icmp : %d\n",sniffer_options.icmp);
+    printf("arp : %d\n",sniffer_options.arp);
+    printf("udp : %d\n",sniffer_options.udp);
+    printf("tcp : %d\n",sniffer_options.tcp);
+    printf("packets count : %d\n",sniffer_options.packet_count);
+    free(sniffer_options.interface);
     return 0;
+}
+
+void initialize_sniffer_options(SnifferOptions *sniffer_options){
+    if(!(sniffer_options->interface = (char *) malloc(MAX_LENGTH))){
+        fprintf(stderr,"Malloc fail\n");
+        exit(2);
+    }
+    sniffer_options->arp = false;
+    sniffer_options->icmp = false;
+    sniffer_options->packet_count = 1;
+    sniffer_options->tcp = false;
+    sniffer_options->udp = false;
+}
+
+void check_arguments(int argc, char *argv[], SnifferOptions *sniffer_options){
+    if (argc>1){
+        int character;
+        while ((character = getopt_long(argc, argv, "i::p:tun:", long_options, NULL)) != -1){
+            switch (character){
+                case 'i':
+                    if(OPTIONAL_ARGUMENT_IS_PRESENT){
+                        strcpy(sniffer_options->interface,optarg);
+                    }else{
+                        printf("Interface bez parametru\n");
+                    }
+                    break;
+                case 'p':{
+                    char *ptr;
+                    int port_number = strtol(optarg, &ptr, 10);
+                    sniffer_options->port_number = port_number;
+                    break;}
+                case 't':
+                    sniffer_options->tcp = true;
+                    break;
+                case 'u':
+                    sniffer_options->udp = true;
+                    break;
+                case 'a':
+                    sniffer_options->arp = true;
+                    break;
+                case 'c':
+                    sniffer_options->icmp = true;
+                    break;
+                case 'n':{
+                    char *ptr;
+                    int packet_count = strtol(optarg, &ptr, 10);
+                    sniffer_options->packet_count = packet_count;
+                    break;}
+                
+                }
+            }
+    }else{
+        fprintf(stderr,"Argument error\n");
+        exit(ARG_ERROR);
+    }
+    
 }
--- sniffer.h
+++ sniffer.h
@@ -7,6 +7,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h> 
+#include <getopt.h>
+
+#define ARG_ERROR 10
+#define MAX_LENGTH 1024
+
+typedef struct sniffer_options_t{
+    char *interface;
+    int port_number;
+    bool tcp;
+    bool udp;
+    bool icmp;
+    bool arp;
+    int packet_count;
+}SnifferOptions;
+
+struct option long_options[] = {
+    {"interface", optional_argument, NULL, 'i'},
+    {"tcp", no_argument, NULL, 't'},
+    {"udp", no_argument, NULL, 'u'},
+    {"arp", no_argument, NULL, 'a'},
+    {"icmp", no_argument, NULL, 'c'},
+    {"num", required_argument, NULL, 'n'},
+    { NULL, 0, NULL, 0}
+};
+
+// https://stackoverflow.com/questions/1052746/getopt-does-not-parse-optional-arguments-to-parameters
+// https://www.binarytides.com/packet-sniffer-code-c-libpcap-linux-sockets/
+// https://stackoverflow.com/questions/7489093/getopt-long-proper-way-to-use-it
+// https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Options.html
+// https://www.itnetwork.cz/cecko/linux/cecko-a-linux-getopt-long-a-shell
+
+#define OPTIONAL_ARGUMENT_IS_PRESENT \
+    ((optarg == NULL && optind < argc && argv[optind][0] != '-') \
+     ? (bool) (optarg = argv[optind++]) \
+     : (optarg != NULL))
+
+void initialize_sniffer_options(SnifferOptions *sniffer_options);
+void check_arguments(int argc, char *argv[], SnifferOptions *sniffer_options);
 
 
 #endif // !SNIFFER_H
